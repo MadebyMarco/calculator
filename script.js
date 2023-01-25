3894949c0fdc377dc6405fca9ff3c8c834930494
MadebyMarco <marco3xmedina@gmail.com>
2023-01-25 10:22:18 -0800

    Realize that you could have used a string instead

--- script.js
+++ script.js
@@ -28,7 +28,6 @@ function operate(setString) {
     const setArray = setString.split(/[+-/*]+/);
     const operation = Array.from(setString);
     const setNumbers = setArray.map(item => parseInt(item, 10));
-    console.log(setArray);
 
     if (operation.includes('+')) { 
        const sum = add(setNumbers[0], setNumbers[1]);
@@ -51,11 +50,16 @@ const display = document.querySelector('.display');
 const numberButtons = document.querySelectorAll('.number');
 let displayArray = [];
 
+let testString = '';
+
+
 numberButtons.forEach(button => button.addEventListener('click', addToDisplay));
 
 function addToDisplay(event) {
     const id = event.target.id;
     displayArray.push(id);
+    testString += id;
+    console.log(testString);
     console.log(displayArray);
     if(displayArray.length == 1) display.textContent = ''; //removes 0 from display
     
@@ -75,12 +79,40 @@ function clearDisplay(){
 const equalsButton = document.querySelector('#equals');
 
 equalsButton.addEventListener('click', () => {
+    loopOperation();
     const displayString = displayArray.join('');
-    console.log(displayString);
-    const result = operate(displayString);
     displayArray = [];
-    display.textContent = result;
-
+    display.textContent = displayString;
 });
-//look at target's id and then display it
 
+let testArr = ['1', '+', '2', '+', '5', '/', '2'];
+let arr = [];
+
+function loopOperation() { //Doesnt account for double digits lmaO 
+    while(displayArray.length != 1) { //determine how many times to run
+
+        for(let i = 0; i < 3; i++) { // Take first three indexes
+        arr.push(displayArray.at(i));
+        }; 
+
+        let arrString = arr.join('');    //Puts first three indexes into operate
+        let operationResult = operate(arrString); 
+
+        displayArray.splice(0, 3, `${operationResult}`); //Puts result back into array
+
+        arr = []; //restarts temporary array for next three indexes
+    };
+};
+
+
+
+
+
+
+
+
+
+//I want to take in the first three indexes in my display array
+//Then I will put them into the operate function
+//then push the result back into the display array
+//want it to stop once it only has one value.
