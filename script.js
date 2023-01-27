761e906955758b7aae8093ceae1f25fc31462ddb
MadebyMarco <marco3xmedina@gmail.com>
2023-01-27 14:01:43 -0800

    Work with multiple operators

--- script.js
+++ script.js
@@ -25,7 +25,8 @@ function divide(a, b) {
 };
 
 function operate(setString) {
-    const setArray = setString.split(/[+-/*]+/);
+
+    const setArray = setString.split(/[/+*-]+/);
     const operation = Array.from(setString);
     const setNumbers = setArray.map(item => parseInt(item, 10));
 
@@ -49,8 +50,10 @@ function operate(setString) {
 const display = document.querySelector('.display');
 const numberButtons = document.querySelectorAll('.number');
 let displayArray = [];
+let result = 0;
 
-let testString = '';
+
+
 
 
 numberButtons.forEach(button => button.addEventListener('click', addToDisplay));
@@ -58,11 +61,14 @@ numberButtons.forEach(button => button.addEventListener('click', addToDisplay));
 function addToDisplay(event) {
     const id = event.target.id;
     displayArray.push(id);
-    testString += id;
-    console.log(testString);
-    console.log(displayArray);
-    if(displayArray.length == 1) display.textContent = ''; //removes 0 from display
+
+        if(id == '+' || id == '-' || id == '/' || id == '*' ) {
+        displayArray.splice((displayArray.length - 1), 0, ' ');
+        displayArray.push(' ');
+        };
     
+    console.log(`display array = ${displayArray}`);
+    if(displayArray.length == 1) display.textContent = ''; //removes 0 from display
     display.textContent += id;
 };
 
@@ -73,46 +79,44 @@ clearButton.addEventListener('click', clearDisplay);
 function clearDisplay(){
     display.textContent = 0;
     displayArray = [];
-    console.log(displayArray);
 };
 
 const equalsButton = document.querySelector('#equals');
 
 equalsButton.addEventListener('click', () => {
-    loopOperation();
-    const displayString = displayArray.join('');
+    const result = loopOperation();
+    display.textContent = result;
     displayArray = [];
-    display.textContent = displayString;
 });
 
-let testArr = ['1', '+', '2', '+', '5', '/', '2'];
 let arr = [];
 
-function loopOperation() { //Doesnt account for double digits lmaO 
-    while(displayArray.length != 1) { //determine how many times to run
 
+function loopOperation() { //Doesnt account for double digits lmaO 
+        const string = displayArray.join('');
+        const stringArray = string.split(' ');
+
+        const operators = displayArray.filter(x => x === '+' || x === '-' || x === '*' || x === '/');
+        console.log(operators)
+
+    for(let c = 0; c < operators.length; c++) { //determine how many times to run
+        
+        console.log(`String array ${stringArray}`);
+        
         for(let i = 0; i < 3; i++) { // Take first three indexes
-        arr.push(displayArray.at(i));
+        arr.push(stringArray.at(i));
         }; 
 
         let arrString = arr.join('');    //Puts first three indexes into operate
+        if(arrString.length == 1) break; 
+        console.log(`operate on ${arrString}`);
         let operationResult = operate(arrString); 
-
-        displayArray.splice(0, 3, `${operationResult}`); //Puts result back into array
+    
+        stringArray.splice(0, 3, `${operationResult}`); //Puts result back into array
 
         arr = []; //restarts temporary array for next three indexes
+
+        
     };
+    return stringArray.join('');
 };
-
-
-
-
-
-
-
-
-
-//I want to take in the first three indexes in my display array
-//Then I will put them into the operate function
-//then push the result back into the display array
-//want it to stop once it only has one value.
